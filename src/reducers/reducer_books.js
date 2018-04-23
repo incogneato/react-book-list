d42fdcda8f13f7eb8a67b04cd0fb61fd06a38fdd
Incogneato <wigginsmichaelc@gmail.com>
2018-04-23 18:46:36 -0400

    add support for redux, book reducer, book_list container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function() {
+  return [
+    { title: 'Javascript: the Good parts' },
+    { title: 'Why\'s Poigniant Guide to Ruby' },
+    { title: 'The Ultimate Guide to Settlers of Catan' },
+  ];
+}
