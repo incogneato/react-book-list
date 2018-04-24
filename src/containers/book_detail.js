c70a84114a629eea75727df64f2f2a89dcdec85b
Incogneato <wigginsmichaelc@gmail.com>
2018-04-23 21:25:13 -0400

    add detail to book detail container, link state etc

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
    render() {
+    if(!this.props.book) {
+      return <div>Select a book to view.</div>;
+    }
+
     return (
         <div>
-          Book detail
+          <h3>hi</h3>
+          <div>{this.props.book.title}</div>
+          <div>{this.props.book.pages}</div>
         </div>
       );
    }
 }
+
+function mapStateToProps(state) {
+  return {
+    book: state.activeBook
+  };
+}
+
+export default connect(mapStateToProps)(BookDetail);
